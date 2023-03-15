1f5a453d61888715d75ce72e243cb11fc1b37407
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-03-15 08:05:55 +0100

    Added progress

---
+++ src/progress.ts
@@ -0,0 +1,53 @@
+import readline from "readline";
+
+export const progressPromises = (
+  title: string,
+  promises: Promise<unknown>[],
+  format: (index: number, resolved: boolean, response: unknown) => string
+) => {
+  let first = true;
+  let numResolved = 0;
+  const isResolved = new Array<boolean>(promises.length);
+  const responses = new Array(promises.length);
+  isResolved.fill(false);
+  responses.fill(null);
+
+  function print() {
+    if (first) {
+      first = false;
+    } else {
+      readline.moveCursor(process.stdout, 0, -promises.length - 2);
+    }
+
+    console.log(`${title}: ${numResolved} / ${promises.length}`);
+
+    for (let i = 0; i < promises.length; ++i) {
+      const resolved = isResolved[i];
+      const response = responses[i];
+      const f = format(i, resolved, response);
+      readline.clearLine(process.stdout, 0);
+      console.log(`    [${resolved ? "X" : " "}] ${f}`);
+    }
+
+    console.log("");
+  }
+
+  print();
+
+  return Promise.all(
+    promises.map(
+      (promise, i) =>
+        new Promise((resolve, reject) => {
+          promise
+            .then((response) => {
+              isResolved[i] = true;
+              responses[i] = response;
+              ++numResolved;
+              print();
+              resolve(response);
+            })
+            .catch(reject);
+        })
+    )
+  );
+};
--- src/test/test.ts
+++ src/test/test.ts
@@ -8,30 +8,31 @@ import { rm } from "../rm";
 import { tempFile } from "../tempFile";
 import { touch } from "../touch";
 import { promptContinue } from "../prompt";
-import { countdown } from "../cron";
+import { countdown, sleep } from "../cron";
 import { grep } from "../grep";
 import { ls } from "../ls";
 import { find, findStr } from "../find";
 import { cp } from "../cp";
+import { progressPromises } from "../progress";
 
 async function run() {
-  //   const res = await $("ls -la");
-  //   console.log(res.stdout);
+  // const res = await $("ls -la");
+  // console.log(res.stdout);
   // console.log(
   //   await fetch(
   //     "https://zagerguitar.com/wp-content/uploads/2015/09/20160322_152654.jpg"
   //   )
   // );
-  //   console.log(await extract("DeDRM_tools_7.2.1.zip", "hello"));
-  //   console.log(await mkdirs("a/b/c"));
-  //   console.log(await mv("DeDRM_tools_7.2.1.zip", "a/b.b"));
-  //   console.log(pwd());
-  //   console.log(await rm("b", { recursive: true }));
-  // console.log(tempFile("stuff"));
+  // console.log(await extract("DeDRM_tools_7.2.1.zip", "hello"));
+  // console.log(await mkdirs("a/b/c"));
+  // console.log(await mv("DeDRM_tools_7.2.1.zip", "a/b.b"));
+  // console.log(pwd());
+  // console.log(await rm("b", { recursive: true }));
+  // console.log(await tempFile("stuff"));
   // console.log(await touch("LICENSE"));
   // console.log(await promptContinue());
   // await sleep(2.5)
-  // after(2.5, () => console.log("after2"));
+  // after(2.5, () => console.log("after"));
   // countdown(6, (left) => console.log(left));
   // console.log(
   //   await grep(/@types/, "package.json", {
@@ -41,6 +42,16 @@ async function run() {
   // console.log(await ls(".", { all: true, long: true }));
   // console.log(await findStr("src", { name: /t/ }));
   // console.log(await cp("src", "b", { recursive: true, force: true }));
+
+  const promises = [sleep(2.5), sleep(1.5), sleep(0.5), sleep(5.5)];
+  await progressPromises(
+    "My progress promises",
+    promises,
+    (i, resolved, response) => {
+      return resolved ? " :) " : " :( " + i;
+    }
+  );
+  console.log("done with progress promises");
 }
 
 run();
